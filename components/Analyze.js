59169753466c959da7de268982d5f0eef6b54eaa
SrishtySoni <052000srishtysoni@gmail.com>
2025-03-14 23:20:32 +0530

    preview, api integration

--- components/Analyze.js
+++ components/Analyze.js
@@ -2,22 +2,23 @@ import Card from "./Card";
 
 const Analyze = ({ data, loading }) => {
   const safeData = Array.isArray(data) ? data : [];
+
   return (
-    <>
-      <div>
-        {loading ? (
-          Array.from({ length: 3 }).map((_, index) => (
-            <Card key={index} loading={loading} data={data} />
-          ))
-        ) : (
-          <div className="flex-1 overflow-y-auto p-4">
-            {safeData.map((item) => (
-              <Card key={item.id} loading={loading} data={item} />
-            ))}
-          </div>
-        )}
-      </div>
-    </>
+    <div className="p-4">
+      {loading ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Card key={index} loading={true} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-8">
+          {safeData.map((item) => (
+            <Card key={item?.scene_number} loading={false} data={item} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
